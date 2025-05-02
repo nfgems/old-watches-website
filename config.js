e95698679374fad7e61fe34020c81352285896b9
nfgems <73572237+nfgems@users.noreply.github.com>
2025-05-02 19:28:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 // config.js
 const EBAY_CONFIG = {
-  // Your eBay API credentials
-  clientId: 'EvanKort-sdw-SBX-72572671c-4789cbc6',
-  // Note: In a production environment, you should never expose your Client Secret in client-side code
-  // This is for demo purposes only. In a real implementation, you would use a secure backend proxy.
-  
   // Your eBay seller information
   sellerId: 'honey_suckle',
   
